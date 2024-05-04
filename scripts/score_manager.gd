c54cf32cae36e6eefd083253fbb92fb6fd4ee290
mwburke <matthew.wesley.burke@gmail.com>
2024-05-04 15:00:23 -0700

    minor changes, backing up

--- scripts/score_manager.gd
+++ scripts/score_manager.gd
@@ -1,9 +1,16 @@
 extends Node
 
 
+@export var score_value : Label
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+
+func reset_scores():
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
